e6274225e783b3ef0f10f2fa5a44a78e68ef6b6d
Stas <stas_sever@mail.ru>
2024-04-16 20:15:05 +0300

    template refactoring and template tag created

--- catalog/views.py
+++ catalog/views.py
@@ -5,23 +5,28 @@ from catalog.models import Product, Contact
 
 def home(request):
     context = {
+        "title": "Главная",
         "object_list": Product.objects.all()
     }
     return render(request, 'catalog/home.html', context)
 
 
 def contacts(request):
-    contact = Contact.objects.get(pk=1)
+    context = {
+        "title": "Контакты",
+        "contact": Contact.objects.get(pk=1)
+    }
     if request.method == 'POST':
         name = request.POST.get('name')
         phone = request.POST.get('phone')
         message = request.POST.get('message')
         print(f"Имя: {name} / Телефон: {phone} / Сообщение: {message}")
-    return render(request, 'catalog/contacts.html', context={"contact": contact})
+    return render(request, 'catalog/contacts.html', context)
 
 
 def product(request, pk):
     context = {
+        "title": "Товар",
         "object": Product.objects.get(pk=pk)
     }
     return render(request, 'catalog/product.html', context)
