41e0ee0f67c3ab082322c0fcde3e30fb795459a9
Stas <stas_sever@mail.ru>
2024-05-20 01:44:20 +0300

    added caching of the entire site

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from django.views.decorators.cache import cache_page
+from django.views.decorators.cache import cache_page, never_cache
 
 from catalog.apps import CatalogConfig
 from catalog.views import ProductListView, ProductDetailView, FeedbackCreateView, ProductCreateView, ProductUpdateView, \
@@ -10,9 +10,9 @@ app_name = CatalogConfig.name
 urlpatterns = [
     path('', ProductListView.as_view(), name="product_list"),
     path('product/<int:pk>', cache_page(60)(ProductDetailView.as_view()), name="product_detail"),
-    path('product/create/', ProductCreateView.as_view(), name="product_create"),
-    path('product/update/<int:pk>', ProductUpdateView.as_view(), name="product_update"),
-    path('product/delete/<int:pk>', ProductDeleteView.as_view(), name="product_delete"),
+    path('product/create/', never_cache(ProductCreateView.as_view()), name="product_create"),
+    path('product/update/<int:pk>', never_cache(ProductUpdateView.as_view()), name="product_update"),
+    path('product/delete/<int:pk>', never_cache(ProductDeleteView.as_view()), name="product_delete"),
     path('contacts/', FeedbackCreateView.as_view(), name="feedback_create"),
     path('category/', CategoryListView.as_view(), name='categories'),
     path('category/<int:pk>', CategoryDetailView.as_view(), name='category_detail')
